1b5a76ffed80b91d3f15bcf29baf918979d26762
Дмитро <124165148+sd13666@users.noreply.github.com>
2023-10-06 14:12:28 +0300

    added Dark Mode

---
+++ script.js
@@ -0,0 +1,18 @@
+let btn = document.getElementById("btn");
+let btnText = document.getElementById("btnText");
+let btnIcon = document.getElementById("btnIcon");
+let footerLogo = document.getElementById("footerLogo");
+
+
+btn.onclick = function () {
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
+        btnIcon.src = "./Images/sun_icon.png";
+        btnText.innerHTML = "Light";
+        footerLogo.src = "./Images/logo_1_Dark.svg";
+    } else {
+        btnIcon.src = "./Images/moon_icon.png";
+        btnText.innerHTML = "Dark";
+        footerLogo.src = "./Images/logo 11.svg"
+    }
+}
