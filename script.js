bdef58fdff8fc74f0e26ea61d35feb537efe8630
Дмитро <124165148+sd13666@users.noreply.github.com>
2023-10-09 15:21:55 +0300

    Dark Mode & repear the logs

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let btnText = document.getElementById("btnText");
 let btnIcon = document.getElementById("btnIcon");
 let footerLogo = document.getElementById("footerLogo");
 
-
 btn.onclick = function () {
     document.body.classList.toggle("dark-theme");
     if (document.body.classList.contains("dark-theme")) {
@@ -13,6 +12,6 @@ btn.onclick = function () {
     } else {
         btnIcon.src = "./Images/moon_icon.png";
         btnText.innerHTML = "Dark";
-        footerLogo.src = "./Images/logo 11.svg"
+        footerLogo.src = "./Images/logo 11.svg";
     }
 }
